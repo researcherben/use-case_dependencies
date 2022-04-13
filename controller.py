b0e5a13424ea48c0be4a31eea755b88fd1c0f771
Ben Payne <ben.is.located@gmail.com>
2022-04-13 06:39:51 -0400

    now with argparse and URLs

--- controller.py
+++ controller.py
@@ -10,6 +10,9 @@
 import os
 import json
 
+# https://realpython.com/python-logging-source-code/
+import argparse  # https://docs.python.org/3.3/library/argparse.html
+
 # https://docs.python.org/3/howto/logging.html
 import logging
 
@@ -37,23 +40,63 @@ def write_to_file(folder_name: str, this_dict: dict, this_prefix: str) -> None:
         json.dump(this_dict, file_handle, indent=2)
     return
 
+def args_parser():
+    theparser = argparse.ArgumentParser(description="generate Graphviz of use cases and user stories from JSON", allow_abbrev=False)
+
+    theparser.add_argument(
+        "--input_filename",
+        metavar="<filename>",
+        type=str,
+        default="use_cases_and_user_stories_and_acceptance_tests.json",
+        help="Name of input JSON file",
+    )
+
+    theparser.add_argument(
+        "--no_output",
+        action="store_true",
+        help="do not create files"
+    )
+    # even though this script is under version control in a git repo,
+    # the --version is useful for when the code base is provided to
+    # a user outside of git
+    theparser.add_argument(
+        "--version", action="store_true", help="version of this script"
+    )
+    # https://semver.org/
+    # MAJOR version when you make incompatible API changes,
+    # MINOR version when you add functionality in a backwards compatible manner, and
+    # PATCH version when you make backwards compatible bug fixes.
+
+    return theparser.parse_args()
+
 
 if __name__ == "__main__":
 
-    # TODO: make this a user argument
-    create_files = True
+    args = args_parser()
+    #print(args)
+
+    if args.version:
+        print("version: 1.0")
+        exit(0)
+
+    # TODO: the color of testable nodes should be set by the result of the test
+    # TODO: the color of top-level non-testable nodes should be set based on the color of lower nodes
+
+    if args.no_output:
+        create_files =False
+    else:
+        create_files = True
 
     folder_name = "output"
     if create_files:
-        os.mkdir(folder_name)
+        if not os.path.exists(folder_name):
+            os.mkdir(folder_name)
 
-    # TODO: replace hardcoded filename with user defined filename as argument
-    with open(
-        "use_cases_and_user_stories_and_acceptance_tests.json", "r"
-    ) as file_handle:
+    with open(args.input_filename, "r") as file_handle:
         data = json.load(file_handle)
     # print(json.dumps(data,indent=4))
 
+    # initialize graphviz data structure
     all_the_things = AGraph(directed=True)
     all_the_things.clear()
     all_the_things.graph_attr["label"] = "Name of graph"
@@ -79,35 +122,51 @@ if __name__ == "__main__":
 
         # the prefixes are only relevant because I have duplicate node names in JSON
         uc_prefix = "use case:"
-        use_cases.add_node(uc_prefix + this_use_case_dict["short name"])
+        uc_filename_prefix="use_case_"
+        use_cases.add_node(uc_prefix + this_use_case_dict["short name"],
+                        URL=folder_name+"/"+uc_filename_prefix+ this_use_case_dict["short name"]+".json",
+                        color="blue")
         if create_files:
-            write_to_file(folder_name, this_use_case_dict, "use_case_")
+            write_to_file(folder_name, this_use_case_dict, uc_filename_prefix)
+
         for this_user_story_dict in this_use_case_dict["user stories"]:
             # print(this_user_story_dict)
             us_prefix = "user story:"
-            user_stories.add_node(us_prefix + this_user_story_dict["short name"])
+            us_filename_prefix="user_story_"
+            user_stories.add_node(us_prefix + this_user_story_dict["short name"],
+                            URL=folder_name+"/"+us_filename_prefix+ this_user_story_dict["short name"]+".json",
+                            color="blue")
             all_the_things.add_edge(
                 uc_prefix + this_use_case_dict["short name"],
                 us_prefix + this_user_story_dict["short name"],
             )
             if create_files:
-                write_to_file(folder_name, this_user_story_dict, "user_story_")
+                write_to_file(folder_name, this_user_story_dict, us_filename_prefix)
 
     for this_acceptance_dict in data["acceptance tests"]:
         acpt_prefix = "acpt:"
-        acceptance.add_node(acpt_prefix + this_acceptance_dict["short name"])
+        acpt_filename_prefix="acpt_"
+        acceptance.add_node(acpt_prefix + this_acceptance_dict["short name"],
+                        URL=folder_name+"/"+acpt_filename_prefix+ this_acceptance_dict["short name"]+".json",
+                        color="blue")
         if create_files:
-            write_to_file(folder_name, this_acceptance_dict, "acpt_")
+            write_to_file(folder_name, this_acceptance_dict, acpt_filename_prefix)
     for this_regression_dict in data["regression tests"]:
         reg_prefix = "reg:"
-        regression.add_node(reg_prefix + this_regression_dict["short name"])
+        reg_filename_prefix = "reg_"
+        regression.add_node(reg_prefix + this_regression_dict["short name"],
+                        URL=folder_name+"/"+reg_filename_prefix+ this_regression_dict["short name"]+".json",
+                        color="blue")
         if create_files:
-            write_to_file(folder_name, this_regression_dict, "reg_")
+            write_to_file(folder_name, this_regression_dict, reg_filename_prefix)
     for this_unit_dict in data["unit tests"]:
         unit_prefix = "unit:"
-        unit.add_node(unit_prefix + this_unit_dict["short name"])
+        unit_filename_prefix = "unit_"
+        unit.add_node(unit_prefix + this_unit_dict["short name"],
+                        URL=folder_name+"/"+unit_filename_prefix+ this_unit_dict["short name"]+".json",
+                        color="blue")
         if create_files:
-            write_to_file(folder_name, this_unit_dict, "unit_")
+            write_to_file(folder_name, this_unit_dict, unit_filename_prefix)
 
     for edge_us_acpt in data["user-story-to-acceptance"]:
         all_the_things.add_edge(
@@ -126,6 +185,7 @@ if __name__ == "__main__":
     all_the_things.draw("all_the_things.svg", format="svg", prog="dot")
 
 
+# this isn't a long-running program, so I'm not using logging
 """
     # maxBytes=10000 = 10kB
     # maxBytes=100000 = 100kB
